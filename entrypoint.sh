bf939263ac2b9794c4ee58fbdebd8faabff45473
Jamie Starke <git@jamiestarke.com>
2026-05-17 00:20:31 +0000

    chore: remove cron from entrypoint and Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,9 @@ if [ "$SKIP_FIREWALL" != "true" ] && [ -f /usr/local/bin/init-firewall.sh ]; the
   /usr/local/bin/init-firewall.sh
 fi
 
-if [ -f /home/appuser/.cron ]; then
-  crontab /home/appuser/.cron
-fi
-
 if [ -n "$GIT_USER_NAME" ] || [ -n "$GIT_USER_EMAIL" ]; then
   gosu appuser git config --global user.name "$GIT_USER_NAME"
   gosu appuser git config --global user.email "$GIT_USER_EMAIL"
 fi
-
-service cron start
 
 exec gosu appuser "$@"
